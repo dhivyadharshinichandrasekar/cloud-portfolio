918d6d2328c68ef8d5a8c846e3a64715a6c78a96
dhivyadharshinichandrasekar <dhivyadharshinic19@gmail.com>
2026-03-03 16:02:34 +0000

    Added project pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-function openProject(name){
-    alert(name + " - Production DevOps Architecture.\n\nBuilt using Docker, Kubernetes, Jenkins, Terraform, SonarQube, Prometheus & Grafana.");
+function openProject(project){
+
+if(project==="smartbuy"){
+window.open("projects/smartbuy.html")
+}
+
+if(project==="screenplay"){
+window.open("projects/screenplay.html")
+}
+
+if(project==="dreamstream"){
+window.open("projects/dreamstream.html")
+}
+
+if(project==="fairytale"){
+window.open("projects/fairytale.html")
+}
+
 }
