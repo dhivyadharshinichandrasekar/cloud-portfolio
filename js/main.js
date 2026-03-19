7ae55956014838a16d55e048ff4476e3e4ed8c00
dhivyadharshinichandrasekar <dhivyadharshinic19@gmail.com>
2026-03-19 14:46:48 +0000

    Level 3 full UI

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 function openProject(project) {
   window.location.href = "projects/" + project + ".html";
 }
+
+const elements = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    if (position < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
