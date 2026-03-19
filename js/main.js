38017e95b1254f91bc4982cdf3dcbd21e5b7b181
dhivyadharshinichandrasekar <dhivyadharshinic19@gmail.com>
2026-03-19 15:23:01 +0000

    FINAL exact neon portfolio

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 function openProject(project) {
-  window.location.href = "projects/" + project + ".html";
+  window.location.href = "./projects/" + project + ".html";
 }
-
-const elements = document.querySelectorAll(".section");
-
-window.addEventListener("scroll", () => {
-  elements.forEach(el => {
-    const position = el.getBoundingClientRect().top;
-    if (position < window.innerHeight - 100) {
-      el.classList.add("show");
-    }
-  });
-});
